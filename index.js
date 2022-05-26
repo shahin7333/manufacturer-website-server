267eff49941dff08b3f4a59f70d903c5f5087cc6
shaparan shahin <shaparan7333@gmail.com>
2022-05-26 16:32:00 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,67 +1,95 @@
 const express = require('express')
-const cors = require('cors');
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb')
 const app = express()
-const port =process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dflzr3e.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dflzr3e.mongodb.net/?retryWrites=true&w=majority`
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-async function run(){
-try{
-await client.connect()
-const toolCollection=client.db('electricalSpark').collection('tools')
-const purchaseCollection=client.db('electricalSpark').collection('purchases')
-const userCollection=client.db('electricalSpark').collection('users')
-
-
-app.get('/tool',async(req,res)=>{
-    const query={}
-    const cursor=toolCollection.find(query)
-    const tools=await cursor.toArray()
-    res.send(tools)
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 })
 
-app.put('/user/:email',async(req,res)=>{
-    const email=req.params.email
-    const user=req.body
-    const filter={email:email}
-    const options = { upsert: true };
-    const updateDoc = {
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' })
+  }
+  const token = authHeader.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded
+    next()
+  })
+}
+
+async function run() {
+  try {
+    await client.connect()
+    const toolCollection = client.db('electricalSpark').collection('tools')
+    const purchaseCollection = client
+      .db('electricalSpark')
+      .collection('purchases')
+    const userCollection = client.db('electricalSpark').collection('users')
+
+    app.get('/tool', async (req, res) => {
+      const query = {}
+      const cursor = toolCollection.find(query)
+      const tools = await cursor.toArray()
+      res.send(tools)
+    })
+
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email
+      const user = req.body
+      const filter = { email: email }
+      const options = { upsert: true }
+      const updateDoc = {
         $set: user,
-      };
-      const result=await userCollection.updateOne(filter,updateDoc,options)
+      }
+      const result = await userCollection.updateOne(filter, updateDoc, options)
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '1h' },
+      )
+      res.send({ result, token })
+    })
+
+    app.get('/ordered', async (req, res) => {
+      const tools = await purchaseCollection.find().toArray()
+      res.send(tools)
+    })
+
+    app.get('/purchase', verifyJWT, async (req, res) => {
+      const customer = req.query.customer
+      const decodedEmail=req.decoded.email
+      if(customer===decodedEmail){
+        const query = { customer: customer }
+        const purchases = await purchaseCollection.find(query).toArray()
+        res.send(purchases)
+      }
+      else{
+          return res.status(403).send({message:'Forbidden Access'})
+      }
+    })
+
+    app.post('/purchase', async (req, res) => {
+      const purchase = req.body
+      const result = await purchaseCollection.insertOne(purchase)
       res.send(result)
-})
-
-app.get('/ordered',async (req,res)=>{
-   const tools=await purchaseCollection.find().toArray()
-    res.send(tools)
-
-})
-
-app.get('/purchase',async(req,res)=>{
- const customer=req.query.customer
- const query={customer:customer}
- const purchases=await purchaseCollection.find(query).toArray()
- res.send(purchases)
-})
-
-app.post('/purchase',async(req,res)=>{
-    const purchase=req.body
-    const result=await purchaseCollection.insertOne(purchase)
-    res.send(result)
-})
-
-}
-finally{
-
-}
+    })
+  } finally {
+  }
 }
 
 run().catch(console.dir)
